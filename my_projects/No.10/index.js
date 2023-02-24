2f1b654f280e498619146201ab4406202bb3c53d
MA RONG <marongreceive@gmail.com>
2023-02-24 22:23:35 +0900

    add project ten

---
+++ my_projects/No.10/index.js
@@ -0,0 +1,32 @@
+const featureImg = document.getElementById('feature_picture')
+const companyImg = document.getElementById('company_picture')
+const feature = document.getElementById('feature')
+const company = document.getElementById('company')
+const dropdownFeature = document.getElementById('dropdown_feature')
+const dropdownCompany = document.getElementById('dropdown_company')
+const arrowUp = "images/icon-arrow-up.svg"
+const arrowDown = "images/icon-arrow-down.svg"
+
+// feature.addEventListener('click',()=> {
+//     if(dropdownFeature.style.display == "block"){
+//         featureImg.src = "images/icon-arrow-down.svg"
+//         dropdownFeature.style.display = "none"
+
+//     }else{
+//             featureImg.src = "images/icon-arrow-up.svg"
+//             dropdownFeature.style.display = "block"
+//             dropdownCompany.style.display = "none"
+//     }
+// })
+
+// company.addEventListener('click',()=> {
+//     if(dropdownCompany.style.display == "block"){
+//         companyImg.src = "images/icon-arrow-down.svg"
+//         dropdownCompany.style.display = "none"
+
+//     }else{
+//             companyImg.src = "images/icon-arrow-up.svg"
+//             dropdownCompany.style.display = "block"
+//             dropdownFeature.style.display = "none"
+//     }
+// })
